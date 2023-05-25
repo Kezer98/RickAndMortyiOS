cb5396b4d7348395a3a8fac3766525ad79b2d6c7
Kieran Fuller <>
2023-05-25 15:28:05 +0100

    Episode 1: Completed - Created Rick And Morty Project

---
+++ RickAndMorty/Controllers/Core/RMEpisodeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RMEpisodeViewController.swift
+//  RickAndMorty
+//
+//  Created by Kieran Fuller on 25/05/2023.
+//
+
+import UIKit
+
+ final class RMEpisodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "Episodes"
+    }
+
+}
---
+++ RickAndMorty/Models/RMEpisode.swift
@@ -0,0 +1,8 @@
+//
+//  RMEpisode.swift
+//  RickAndMorty
+//
+//  Created by Kieran Fuller on 25/05/2023.
+//
+
+import Foundation
