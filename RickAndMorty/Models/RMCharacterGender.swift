bfb7703ed5b02865e8d0dec6eff93ab7e43ce6de
Kieran Fuller <>
2023-06-15 15:05:25 +0100

    Episode 3 Completed: Add Models

---
+++ RickAndMorty/Models/RMCharacterGender.swift
@@ -0,0 +1,15 @@
+//
+//  RMCharacterGender.swift
+//  RickAndMorty
+//
+//  Created by Kieran Fuller on 15/06/2023.
+//
+
+import Foundation
+
+enum RMCharacterGender: String, Codable {
+    case male = "Male"
+    case female = "Female"
+    case genderless = "Genderless"
+    case `unknown` = "Unknown"
+}
